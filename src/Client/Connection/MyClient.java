2cd85eea4cbd76a7165de5193d2f0ccea3d9c310
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-28 17:56:51 -0400

    Initial commit

---
+++ src/Client/Connection/MyClient.java
@@ -0,0 +1,39 @@
+package Client.Connection;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MyClient extends AbstractClient {
+
+	public MyClient(String host, int port)
+	  { 
+	    super(host, port); 
+	}
+
+	@Override
+	protected void handleMessageFromServer(Object msg) {
+		System.out.println(String.format("Server: " + msg));
+	}
+
+	public static void main(String[] args) throws IOException   {
+		MyClient Client1 = new MyClient("localhost", 8989);
+		String s;
+		Scanner myObj = new Scanner(System.in);
+		try {
+			Client1.openConnection();
+		} catch (IOException e1) { 
+			e1.printStackTrace();
+			System.out.println("ERROR while attempting to open connection, exiting...");
+			System.exit(1); // on error exit
+		}
+		while(true) {
+			try {
+				s = myObj.nextLine();
+				Client1.sendToServer(s);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	    
+	} 
+}
---
+++ src/Server/Connection/MyServer.java
@@ -0,0 +1,44 @@
+package Server.Connection;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MyServer extends AbstractServer  {
+ 
+	public MyServer(int port)   { // Constructor
+		super(port); 
+	}
+	
+	@Override
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+			System.out.println(String.format("Client " + client.getName() + ": " + msg));
+	} 
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static void main(String[] args)   {
+		// TODO Auto-generated method stub
+		String msg;
+		Scanner myObj = new Scanner(System.in);
+		MyServer MyServer1 = new MyServer(8989); // will use port 8989
+		System.out.println("MyServer: MyServer1 has been created.");
+		try {
+			MyServer1.listen();
+			System.out.println("MyServer: Server should now be running");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(true) {
+			msg = myObj.nextLine();
+			try {
+				MyServer1.sendToAllClients(msg);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	} 
+}
