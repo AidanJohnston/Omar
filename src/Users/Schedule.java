c76f86d3e0726033fa95f18c8485f40f4d20104a
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 15:26:33 -0400

    Added consturctor and setters and getters
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Users/Schedule.java
+++ src/Users/Schedule.java
@@ -1,4 +1,21 @@
 package Users;
 
+import java.util.List;
+
 public class Schedule {
+    private List<Appointment> appointmentList;
+
+    public Schedule() {
+
+        //DO NOTHING
+
+    }
+
+    public List<Appointment> getAppointmentList() {
+        return appointmentList;
+    }
+
+    public void addAppointment(Appointment appointment) {
+        this.appointmentList.add(appointment);
+    }
 }
