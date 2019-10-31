ff2476c8e92c16866cf08427c39844c8ece7736e
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 15:01:22 -0400

    Created Doctor.java
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

---
+++ src/Users/Doctor.java
@@ -0,0 +1,10 @@
+package Users;
+
+public class Doctor {
+    private String speciatly;
+    private String rank;
+    private double startHour;
+    private double endHour;
+    private Schedule schedule;
+    private List<Patient> patients;
+}
