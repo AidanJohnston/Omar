e051b5c0079410fb5ab4a1737d0496d61c0966d3
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 17:00:21 -0400

    Updated javadoc
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Users/Staff.java
+++ src/Users/Staff.java
@@ -2,7 +2,26 @@ package Users;
 
 import java.util.Date;
 
+/**
+ * Staff.java - A class for storing information relative to staff members
+ * @author Aidan Johnston
+ * @version 1.0
+ * @see User
+ */
 public class Staff extends User{
+
+    /**
+     * Constructor for the Staff class
+     * @param username
+     * @param password
+     * @param Fname
+     * @param LName
+     * @param brithday
+     * @param SIN
+     * @param adress
+     * @param ID
+     * @param phoneNumber
+     */
     public Staff(
             String username,
             String password,
