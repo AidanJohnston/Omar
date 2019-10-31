21a8ad16bfc4e91bc685cc07bb78757fc11cf8ce
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 15:07:38 -0400

    Added consturctor and setters and getters
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Users/Doctor.java
+++ src/Users/Doctor.java
@@ -1,5 +1,7 @@
 package Users;
 
+import java.util.List;
+
 public class Doctor {
     private String speciatly;
     private String rank;
@@ -7,4 +9,65 @@ public class Doctor {
     private double endHour;
     private Schedule schedule;
     private List<Patient> patients;
+
+    public Doctor(String speciatly, String rank, double startHour, double endHour, Schedule schedule, List<Patient> patients) {
+        this.speciatly = speciatly;
+        this.rank = rank;
+        this.startHour = startHour;
+        this.endHour = endHour;
+        this.schedule = schedule;
+        this.patients = patients;
+    }
+
+    public String getSpeciatly() {
+        return speciatly;
+    }
+
+    public void setSpeciatly(String speciatly) {
+        this.speciatly = speciatly;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public double getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(double startHour) {
+        this.startHour = startHour;
+    }
+
+    public double getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(double endHour) {
+        this.endHour = endHour;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
+
+    public void addCurrentPatient(Patient patient) {
+        this.patients.add(patient);
+    }
 }
