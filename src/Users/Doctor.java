be2f604b6fd817ea26e0b5e49b7b54af4edb9fce
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 16:44:39 -0400

    Updated javadoc
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Users/Doctor.java
+++ src/Users/Doctor.java
@@ -3,6 +3,12 @@ package Users;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Doctor.java - A class for storing information relative to doctors
+ * @author Aidan Johnston
+ * @version 1.0
+ * @see User
+ */
 public class Doctor extends User{
     private String speciatly;
     private String rank;
@@ -11,6 +17,24 @@ public class Doctor extends User{
     private Schedule schedule;
     private List<Patient> patients;
 
+    /**
+     * Constructor for the Doctor class
+     * @param username
+     * @param password
+     * @param Fname
+     * @param LName
+     * @param brithday
+     * @param SIN
+     * @param adress
+     * @param ID
+     * @param phoneNumber
+     * @param speciatly
+     * @param rank
+     * @param startHour
+     * @param endHour
+     * @param schedule
+     * @param patients
+     */
     public Doctor(
                 String username,
                 String password,
@@ -36,54 +60,102 @@ public class Doctor extends User{
         this.patients = patients;
     }
 
+    /**
+     * Gets the doctors speciatly
+     * @return speciatly
+     */
     public String getSpeciatly() {
         return speciatly;
     }
 
+    /**
+     * Sets the doctors speciatly
+     * @param speciatly
+     */
     public void setSpeciatly(String speciatly) {
         this.speciatly = speciatly;
     }
 
+    /**
+     * Gets the doctors rank
+     * @return rank
+     */
     public String getRank() {
         return rank;
     }
 
+    /**
+     * Sets the doctor rank
+     * @param rank
+     */
     public void setRank(String rank) {
         this.rank = rank;
     }
 
+    /**
+     * Gets the doctors starts hours for work
+     * @return startHours
+     */
     public double getStartHour() {
         return startHour;
     }
 
+    /**
+     * Sets the doctors start time
+     * @param startHour
+     */
     public void setStartHour(double startHour) {
         this.startHour = startHour;
     }
 
+    /**
+     * Gets the doctors end time
+     * @return endHour
+     */
     public double getEndHour() {
         return endHour;
     }
 
+    /**
+     * Sets the doctors end work hours
+     * @param endHour
+     */
     public void setEndHour(double endHour) {
         this.endHour = endHour;
     }
 
+    /**
+     * Gets the doctor schedule
+     * @return schedule
+     * @see Schedule
+     */
     public Schedule getSchedule() {
         return schedule;
     }
 
+    /**
+     * Sets the doctors schedule
+     * @param schedule
+     * @see Schedule
+     */
     public void setSchedule(Schedule schedule) {
         this.schedule = schedule;
     }
 
+    /**
+     * Gets a list of the doctors patients, included all patients.  Current, pass, and future
+     * @return List of Patients
+     * @see Patient
+     */
     public List<Patient> getPatients() {
         return patients;
     }
 
-    public void setPatients(List<Patient> patients) {
-        this.patients = patients;
-    }
-
+    /**
+     * Added a patient to the doctors list of patients
+     * @param patient
+     * @see Patient
+     */
     public void addCurrentPatient(Patient patient) {
         this.patients.add(patient);
     }
