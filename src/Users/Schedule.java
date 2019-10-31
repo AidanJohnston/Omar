f415d01c52e063fc1c189e817d63f52130c0441b
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 16:58:50 -0400

    Updated javadoc
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Users/Schedule.java
+++ src/Users/Schedule.java
@@ -2,19 +2,35 @@ package Users;
 
 import java.util.List;
 
+/**
+ * Schedule.java - A class for store a list of appointments
+ * @author Aidan Johnston
+ * @version 1.0
+ */
 public class Schedule {
     private List<Appointment> appointmentList;
 
+    /**
+     * Constructor for the Schedule class
+     */
     public Schedule() {
 
         //DO NOTHING
 
     }
 
+    /**
+     * Gets the List of appointments
+     * @return List of appointments
+     */
     public List<Appointment> getAppointmentList() {
         return appointmentList;
     }
 
+    /**
+     * Adds an appointment to the appointment list
+     * @param appointment
+     */
     public void addAppointment(Appointment appointment) {
         this.appointmentList.add(appointment);
     }
