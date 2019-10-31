7e64f78fa54c8d86c8eebcf2029d88e9fdc03b57
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 15:22:52 -0400

    Added consturctor and setters and getters
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Users/Appointment.java
+++ src/Users/Appointment.java
@@ -1,4 +1,48 @@
 package Users;
 
 public class Appointment {
+
+    private Patient patient;
+    private Doctor doctor;
+    private Diagnosis diagnosis;
+    private Prescription prescription;
+
+    public Appointment(Patient patient, Doctor doctor, Diagnosis diagnosis, Prescription prescription) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.diagnosis = diagnosis;
+        this.prescription = prescription;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Diagnosis getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(Diagnosis diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public Prescription getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(Prescription prescription) {
+        this.prescription = prescription;
+    }
 }
--- src/Users/Prescription.java
+++ src/Users/Prescription.java
@@ -1,4 +1,49 @@
 package Users;
 
+import java.util.Date;
+
 public class Prescription {
+    private String name;
+    private Date date;
+    private String note;
+    private String instructions;
+
+    public Prescription(String name, Date date, String note, String instructions) {
+        this.name = name;
+        this.date = date;
+        this.note = note;
+        this.instructions = instructions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
 }
