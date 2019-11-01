897720e9d615abbe1ac74d7797e6c389bc760450
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 22:14:47 -0400

    Setup ClientSecretary to handle talking between myClient and muGUI
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Clinic/Client/ClientSecretary.java
+++ src/Clinic/Client/ClientSecretary.java
@@ -1,14 +1,34 @@
 package Clinic.Client;
 
+import Clinic.Client.Connection.MyClient;
+import Clinic.Client.GUI.MyGUI;
 import Clinic.Payload;
 
 public class ClientSecretary {
+
+    MyClient myClient;
+    MyGUI myGUI;
+
     public ClientSecretary() {
-
-
-
     }
-    public void handleMessageFromServer(Object payload) {
-        Payload payload = (Payload) payload;
+
+    public MyClient getMyClient() {
+        return myClient;
+    }
+
+    public void setMyClient(MyClient myClient) {
+        this.myClient = myClient;
+    }
+
+    public MyGUI getMyGUI() {
+        return myGUI;
+    }
+
+    public void setMyGUI(MyGUI myGUI) {
+        this.myGUI = myGUI;
+    }
+
+    public void handleMessageFromServer(Object payloadFromServer) {
+        Payload payload = (Payload) payloadFromServer;
     }
 }
