4e89ec6cb6ac8fb94d6d9b8f655df93d87eb0672
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 21:03:12 -0400

    Initial commit
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

---
+++ src/Client/Client.java
@@ -0,0 +1,8 @@
+package Client;
+
+public class Client {
+
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,8 @@
+package Server;
+
+public class Server {
+
+    public static void main(String args[]) {
+
+    }
+}
