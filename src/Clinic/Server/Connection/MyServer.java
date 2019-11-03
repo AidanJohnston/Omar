6871e9615d59683fd3ad37faef95fbd3ac9887f0
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-03 14:54:12 -0500

    Passes information to ServerSecretary
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Clinic/Server/Connection/MyServer.java
+++ src/Clinic/Server/Connection/MyServer.java
@@ -1,45 +1,23 @@
 package Clinic.Server.Connection;
 
 import Clinic.Core.Payload;
+import Clinic.Server.ServerSecretary;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MyServer extends AbstractServer  {
- 
-	public MyServer(int port)   { // Constructor
-		super(port); 
+
+	private ServerSecretary serverSecretary;
+
+	public MyServer(int port, ServerSecretary serverSecretary)   { // Constructor
+		super(port);
+
+		this.serverSecretary = serverSecretary;
 	}
 	
 	@Override
-	protected void handleMessageFromClient(Object msg, ConnectionToClient client) throws InterruptedException {
-	    Payload payload = (Payload) msg;
-        if(payload.getType() == 1) {
-            try {
-                client.sendToClient(new Payload(payload.getId(), 1, (Object) "Orange"));
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-	} 
-	
-	//////////////////////////////////////////////////////////////////////////////////////////
-
-	//TODO Re write main as constructor
-	public static void main(String[] args)   {
-		String msg;
-		Scanner myObj = new Scanner(System.in);
-		MyServer MyServer1 = new MyServer(6969); // will use port 8989
-		System.out.println("MyServer: MyServer1 has been created.");
-		try {
-			MyServer1.listen();
-			System.out.println("MyServer: Clinic.Server should now be running");
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	} 
+	protected void handleMessageFromClient(Object payload, ConnectionToClient client) {
+		serverSecretary.handleMessageFromClient((Payload) payload, client);
+	}
 }
