c64d1e1d1ea4a1a5bca078afa61012a7ab969606
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-05 13:39:24 -0500

    TESTCCOMMITMYDUDE
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Clinic/Server/Server.java
+++ src/Clinic/Server/Server.java
@@ -1,8 +1,13 @@
 package Clinic.Server;
 
+import Clinic.Server.Connection.MyServer;
+
+import java.io.Serializable;
+
 public class Server {
 
     public static void main(String args[]) {
-
+        ServerSecretary serverSecretary = new ServerSecretary();
+        MyServer myServer = new MyServer(6969, serverSecretary);
     }
 }
