7f53af0f0a7c95d6ebe05549d4a8f6ddc6de00cc
Dawson Gilmore <d.gilmore@socyinc.com>
2019-11-12 09:30:40 -0500

    Director has methods now, they're probably named wrong but w/e

--- src/Clinic/Server/ServerDirector.java
+++ src/Clinic/Server/ServerDirector.java
@@ -6,7 +6,61 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ServerDirector {
-    public void login(Object params){
-        
+    public Object update(Object params){
+        return null;
+    }
+    public Object login(Object params){
+        return null;
+    }
+    public Object logout(Object params){
+        return null;
+    }
+    public Object error(Object params){
+        return null;
+    }
+    public Object diagnosisGetAllPatient(Object params){
+        return null;
+    }
+    public Object diagnosisGetAllDoctor(Object params){
+        return null;
+    }
+    public Object diagnosisUpdateGivenReference(Object params){
+        return null;
+    }
+    public Object diagnosisCreateGivenReference(Object params){
+        return null;
+    }
+    public Object diagnosisRemoveGivenReference(Object params){
+        return null;
+    }
+    public Object doctorGetGivenPatient(Object params){
+        return null;
+    }
+    public Object doctorGetAll(Object params){
+        return null;
+    }
+    public Object doctorGetGivenId(Object params){
+        return null;
+    }
+    public Object doctorUpdateGivenDoctor(Object params){
+        return null;
+    }
+    public Object doctorCreateGivenDoctor(Object params){
+        return null;
+    }
+    public Object doctorRequestGivenName(Object params){
+        return null;
+    }
+    public Object doctorRequestGivenTime(Object params){
+        return null;
+    }
+    public Object doctorRequestGivenDate(Object params){
+        return null;
+    }
+    public Object doctorRemoveGivenDoctor(Object params){
+        return null;
+    }
+    public Object patientGetAll(Object params){
+        return null;
     }
 }
