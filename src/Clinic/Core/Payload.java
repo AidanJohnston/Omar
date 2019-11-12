88c053d94e54365be05c70aedfc320d4a8fa2877
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-12 13:52:50 -0500

    Refactored payload Removed all setters
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Clinic/Core/Payload.java
+++ src/Clinic/Core/Payload.java
@@ -128,31 +128,15 @@ public class Payload implements Serializable {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public Object getObject() {
         return object;
     }
 
-    public void setObject(Object object) {
-        this.object = object;
-    }
-
     public Token getToken() {
         return token;
-    }
-
-    public void setToken(Token token) {
-        this.token = token;
     }
 }
