3fbe897cace989e7a3f69c25f7fc6247eaa4afd7
Dawson Gilmore <d.gilmore@socyinc.com>
2019-11-19 16:39:36 -0500

    ahahahahahahahaha we're writing now bois (MAYBE MAYBE noT)

--- src/Clinic/Server/Data/DataWriter.java
+++ src/Clinic/Server/Data/DataWriter.java
@@ -5,39 +5,32 @@ import Clinic.Core.Doctor;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class DataWriter {
-    private String writeTag(String tagName, String value){
+    private String writeTag(String tagName, String value) {
         return "<" + tagName + ">" + value + "</" + tagName + ">\r\n";
     }
 
-    private String indent(int i){
+    private String indent(int i) {
         String s = "";
-        for(int x = 0; x < i; x++){
+        for (int x = 0; x < i; x++) {
             s += "\t";
         }
         return s;
-    }    
+    }
 
-    public void writeDoctors(ArrayList<Doctor> doctors){
-        try{
-            OutputStream fout= new FileOutputStream("doctors.xml"); 
-            OutputStream bout= new BufferedOutputStream(fout); 
-            OutputStreamWriter outfile = new OutputStreamWriter(bout, "8859_1");
-            outfile.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n");
-            for(Doctor d : doctors){
-                outfile.write("<Doctor>\r\n");
-                outfile.write(indent(1));
-                outfile.write(writeTag("FName", d.getFName()));
-
-            }        
-            outfile.flush();
-            outfile.close();
-            }catch (UnsupportedEncodingException e) {
+    public void writeDoctors(ArrayList<Doctor> doctors) throws IllegalArgumentException, IllegalAccessException {
+        try {
+            FileOutputStream fout = new FileOutputStream("doctors.xml");
+            ObjectOutputStream oout = new ObjectOutputStream(fout);
+            oout.writeObject(doctors);
+        }catch (UnsupportedEncodingException e) {
                 System.out.println("This VM does not support the Latin-1 character set.");
             }catch (IOException e) {
                 System.out.println(e.getMessage());
--- src/Clinic/Server/ServerDirector.java
+++ src/Clinic/Server/ServerDirector.java
@@ -1,17 +1,18 @@
 package Clinic.Server;
 
 import Clinic.Core.*;
+import Clinic.Server.Data.DataWriter;
 import Util.*;
 import Util.Exceptions.IncorrectPayloadException;
 
 import java.util.ArrayList;
 
 public class ServerDirector {
-    /*  This should stay dead, but just in case everything goes wrong and we need to do this  
-    public Method login(){
-        return this.getClass().getEnclosingMethod();
-    }
-    //*/
+    /*
+     * This should stay dead, but just in case everything goes wrong and we need to
+     * do this public Method login(){ return this.getClass().getEnclosingMethod(); }
+     * //
+     */
     public Token login(Util.PayloadBoys.login l) throws IncorrectPayloadException {
         UserType type = UserType.DOCTOR; // REPLACE WITH DATABASE LOOKUP
         int userID = 1;
@@ -20,14 +21,31 @@ public class ServerDirector {
 
         return token;
     }
-    public Object logout(Object params)throws IncorrectPayloadException {
+
+    public Object logout(Object params) throws IncorrectPayloadException {
         return null;
     }
-    public Object createAppointment(Object params) throws IncorrectPayloadException {return null;}
-    public Object getAllSchedule(Object params) throws IncorrectPayloadException {return null;}
-    public Object getScheduleOfDoctor(Object params) throws IncorrectPayloadException {return null;}
-    public Object getCurrentAppointmentOfPatient(Object params) throws IncorrectPayloadException {return null;}
-    public Object getAllAppointmentOfPatient(Object params) throws IncorrectPayloadException {return null;}
+
+    public Object createAppointment(Object params) throws IncorrectPayloadException {
+        return null;
+    }
+
+    public Object getAllSchedule(Object params) throws IncorrectPayloadException {
+        return null;
+    }
+
+    public Object getScheduleOfDoctor(Object params) throws IncorrectPayloadException {
+        return null;
+    }
+
+    public Object getCurrentAppointmentOfPatient(Object params) throws IncorrectPayloadException {
+        return null;
+    }
+
+    public Object getAllAppointmentOfPatient(Object params) throws IncorrectPayloadException {
+        return null;
+    }
+
     public Object getAllDoctor() throws IncorrectPayloadException {
 
         ArrayList<Doctor> list = new ArrayList<>();
@@ -36,7 +54,12 @@ public class ServerDirector {
         list.add(new Doctor("Dawson"));
         list.add(new Doctor("John"));
         list.add(new Doctor("Mai"));
-
+        DataWriter d = new DataWriter();
+        try {
+            d.writeDoctors(list);
+        } catch (Exception e) {
+            return e;
+        }
         return list;
     }
     public Object getDoctorWithId(Object params) throws IncorrectPayloadException {return null;}
