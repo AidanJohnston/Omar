c98cad6251f3a71554ee11f5f609fc02bea2d25b
Dawson Gilmore <dfgilmor@lakeheadu.ca>
2019-11-24 10:57:24 -0500

    Read and write staff and patients

--- src/Clinic/Server/Data/DataReader.java
+++ src/Clinic/Server/Data/DataReader.java
@@ -1,17 +1,11 @@
 package Clinic.Server.Data;
 
-import Clinic.Core.Doctor;
+import Clinic.Core.*;
 
-import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class DataReader {
@@ -27,5 +21,31 @@ public class DataReader {
                 System.out.println(e.getMessage());
             }
         return doctors;
+    }
+    public ArrayList<Patient> readPatients() throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
+        ArrayList<Patient> patients = null;
+        try {
+            FileInputStream fout = new FileInputStream("patients.xml");
+            ObjectInputStream oout = new ObjectInputStream(fout);
+            patients = (ArrayList<Patient>)oout.readObject();
+        }catch (UnsupportedEncodingException e) {
+                System.out.println("This VM does not support the Latin-1 character set.");
+            }catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        return patients;
+    }
+    public ArrayList<Staff> readStaff() throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
+        ArrayList<Staff> staff = null;
+        try {
+            FileInputStream fout = new FileInputStream("staff.xml");
+            ObjectInputStream oout = new ObjectInputStream(fout);
+            staff = (ArrayList<Staff>)oout.readObject();
+        }catch (UnsupportedEncodingException e) {
+                System.out.println("This VM does not support the Latin-1 character set.");
+            }catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        return staff;
     }
 }
--- src/Clinic/Server/Data/DataWriter.java
+++ src/Clinic/Server/Data/DataWriter.java
@@ -1,6 +1,6 @@
 package Clinic.Server.Data;
 
-import Clinic.Core.Doctor;
+import Clinic.Core.*;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
@@ -18,5 +18,14 @@ public class DataWriter {
             ObjectOutputStream oout = new ObjectOutputStream(fout);
             oout.writeObject(doctors);
     }
-    
+    public void writePatients(ArrayList<Patient> patients) throws IllegalArgumentException, IllegalAccessException, IOException {
+        FileOutputStream fout = new FileOutputStream("patients.xml");
+        ObjectOutputStream oout = new ObjectOutputStream(fout);
+        oout.writeObject(patients);
+    }
+    public void writeStaff(ArrayList<Staff> staff) throws IllegalArgumentException, IllegalAccessException, IOException {
+        FileOutputStream fout = new FileOutputStream("patients.xml");
+        ObjectOutputStream oout = new ObjectOutputStream(fout);
+        oout.writeObject(staff);
+    }
 }
