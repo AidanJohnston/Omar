32659f73fbc1948bc54b9fd8fbb5ace8b01a59e3
Aidan Johnston <aidanjhnston@gmail.com>
2019-12-01 18:21:37 -0500

    Health care is type local date, removed schedule

--- src/Clinic/Core/Patient.java
+++ src/Clinic/Core/Patient.java
@@ -15,16 +15,11 @@ import java.util.List;
 public class Patient extends User implements Serializable {
 
     private int healthCardNumber;
-    private Date healthCardExpiry;
+    private LocalDate healthCardExpiry;
     private String province;
-    private Schedule schedule;
-    private List<Diagnosis> diagnoses;
-    private List<Prescription> prescriptions;
 
     /**
      * Constructor for the patient class
-     * @param username
-     * @param password
      * @param FName
      * @param LName
      * @param brithday
@@ -35,13 +30,8 @@ public class Patient extends User implements Serializable {
      * @param healthCardNumber
      * @param healthCardExpiry
      * @param province
-     * @param schedule
-     * @param diagnoses
-     * @param prescriptions
      */
     public Patient(
-            String username,
-            String password,
             String FName,
             String LName,
             LocalDate brithday,
@@ -50,18 +40,12 @@ public class Patient extends User implements Serializable {
             int ID,
             String phoneNumber,
             int healthCardNumber,
-            Date healthCardExpiry,
-            String province,
-            Schedule schedule,
-            List<Diagnosis> diagnoses,
-            List<Prescription> prescriptions) {
+            LocalDate healthCardExpiry,
+            String province) {
         super(FName, LName, brithday, SIN, adress, ID, phoneNumber);
         this.healthCardNumber = healthCardNumber;
         this.healthCardExpiry = healthCardExpiry;
         this.province = province;
-        this.schedule = schedule;
-        this.diagnoses = diagnoses;
-        this.prescriptions = prescriptions;
     }
 
     public Patient(int id) {
@@ -95,7 +79,7 @@ public class Patient extends User implements Serializable {
      *
      * @return
      */
-    public Date getHealthCardExpiry() {
+    public LocalDate getHealthCardExpiry() {
         return healthCardExpiry;
     }
 
@@ -104,7 +88,7 @@ public class Patient extends User implements Serializable {
      *
      * @param healthCardExpiry
      */
-    public void setHealthCardExpiry(Date healthCardExpiry) {
+    public void setHealthCardExpiry(LocalDate healthCardExpiry) {
         this.healthCardExpiry = healthCardExpiry;
     }
 
@@ -126,82 +110,4 @@ public class Patient extends User implements Serializable {
         this.province = province;
     }
 
-    /**
-     * Gets the schedule of the patients upcoming appointments
-     *
-     * @return schedule
-     * @see Schedule
-     */
-    public Schedule getSchedule() {
-        return schedule;
-    }
-
-    /**
-     * Sets the schedule of upcoming appointments for the patient
-     *
-     * @param schedule
-     */
-    public void setSchedule(Schedule schedule) {
-        this.schedule = schedule;
-    }
-
-    /**
-     * Gets a list of all diagnoses given to the patient
-     *
-     * @return diagnoses
-     * @see Diagnosis
-     */
-    public List<Diagnosis> getDiagnoses() {
-        return diagnoses;
-    }
-
-    /**
-     * Sets a list of diagnoses given to the patient
-     *
-     * @param diagnoses
-     * @see Diagnosis
-     */
-    public void setDiagnoses(List<Diagnosis> diagnoses) {
-        this.diagnoses = diagnoses;
-    }
-
-    /**
-     * Gets the list of all prescriptions given to the patient
-     *
-     * @return prescriptions
-     * @see Prescription
-     */
-    public List<Prescription> getPrescriptions() {
-        return prescriptions;
-    }
-
-    /**
-     * Sets a list of prescriptions given to the patient
-     *
-     * @param prescriptions
-     * @see Prescription
-     */
-    public void setPrescriptions(List<Prescription> prescriptions) {
-        this.prescriptions = prescriptions;
-    }
-
-    /**
-     * Added a diagnosis to the diagnosis list
-     *
-     * @param diagnosis
-     * @see Diagnosis
-     */
-    public void addDiagnose(Diagnosis diagnosis) {
-        this.diagnoses.add(diagnosis);
-    }
-
-    /**
-     * Added a prescription to the prescription list
-     *
-     * @param prescription
-     * @see Prescription
-     */
-    public void addPrescription(Prescription prescription) {
-        this.prescriptions.add(prescription);
-    }
 }
