cbb38b6c73e7c2242779602deedf47d65dfc2c89
Aevo55 <dawspuddingcat@gmail.com>
2019-12-01 14:35:49 -0500

    Hashing

--- src/Clinic/Core/Credentials.java
+++ src/Clinic/Core/Credentials.java
@@ -1,8 +1,9 @@
 package Clinic.Core;
 
-
 import java.io.Serializable;
 import java.lang.reflect.Type;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import Util.UserType;
 
@@ -12,14 +13,14 @@ public class Credentials implements Serializable{
     private int userID;
     private UserType type;
 
-    public Credentials(String _username, String _hashword){
+    public Credentials(String _username, String password){
         username = _username;
-        hashword = _hashword;
+        hashword = hashPassword(password);
     }
 
-    public Credentials(String _username, String _hashword, int _userID, UserType _type){
+    public Credentials(String _username, String password, int _userID, UserType _type){
         username = _username;
-        hashword = _hashword;
+        hashword = hashPassword(password);
         userID = _userID;
         type = _type;
     }
@@ -38,5 +39,30 @@ public class Credentials implements Serializable{
 
     public UserType getType(){
         return type;
+    }
+
+    private String hashPassword(String passwordToHash){
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+            //Get the hash's bytes 
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            return sb.toString();
+        } 
+        catch (NoSuchAlgorithmException e) 
+        {
+            e.printStackTrace();
+            return passwordToHash;
+        }
     }
 }
