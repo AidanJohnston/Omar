732be923f63c5dc5080db8d40364f3f6223ed364
Aidan Johnston <aidanjhnston@gmail.com>
2019-12-01 18:23:04 -0500

    More things in DataPopulate

--- src/Clinic/Server/Data/DataPopulate.java
+++ src/Clinic/Server/Data/DataPopulate.java
@@ -4,6 +4,7 @@ import Clinic.Core.*;
 import Util.UserType;
 import Util.Exceptions.LoginFailedException;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class DataPopulate {
@@ -11,10 +12,6 @@ public class DataPopulate {
         popDoc();
         popCreds();
     }
-
-    private static void popApps(){
-        
-    }    
 
     private static void popCreds(){
         ArrayList<Credentials> creds = new ArrayList<>();
@@ -31,11 +28,9 @@ public class DataPopulate {
 
     private static void popDoc(){
         ArrayList<Doctor> list = new ArrayList<>();
-        list.add(new Doctor("Aidan"));
-        list.add(new Doctor("Sean"));
-        list.add(new Doctor("Dawson"));
-        list.add(new Doctor("John"));
-        list.add(new Doctor("Mai"));
+        list.add(new Doctor("Orange", "Lube", LocalDate.of(1999, 5, 27), 420, "69 Hello Street", 129, "8076303284", "Fucking Bitches", "Head Doctor", 0, 2));
+        list.add(new Doctor("Sean", "Dexor", LocalDate.of(1999, 7, 23), 403, "69 Suck my ass Street", 265, "87630483", "Fucking Biteches", "Second Doctor", 0, 1));
+
         DataWriter d = new DataWriter();
         //write some example info to the database xml
         try {
@@ -43,6 +38,34 @@ public class DataPopulate {
         } catch (Exception e) {
             System.out.println("Writer broke");
         }
+    }
+
+    private static void popPatient() {
+        ArrayList<Patient> list = new ArrayList<>();
+        list.add(new Patient("Dawson", "Aevo", LocalDate.of(2000, 3, 26 ), 696969, "420 oh heck yeah Street", 14, "80-7259803", 2525, LocalDate.of(2020, 1, 12), "ON"));
+        list.add(new Patient("Aidan", "Johnston", LocalDate.of(1999, 5, 25), 2525, "Hey there this is an adress",  52352, "607-2343", 242, LocalDate.of(2020, 12, 23), "ON"));
+
+        DataWriter d = new DataWriter();
+        //write some example info to the database xml
+        try {
+            d.writePatients(list);
+        } catch (Exception e) {
+            System.out.println("Writer broke");
+        }
+    }
+
+    private static  void popPrescriptions() {
+        ArrayList<Prescription>  list = new ArrayList<>();
+
+        //list.add(new Prescription("Drug", LocalDate.now(), "This is a drug that cures being sick", "Take every  ten minutes", ))
+    }
+
+    private static void popAppointment() {
+
+        ArrayList<Appointment> list = new ArrayList<>();
+     //   list.add(new Appointment(new DataReader().readPatients().get(0), new DataReader().readDoctors().get(0), LocalDate.of(2019, 12, 2), new Diagnosis(), new Prescription()));
+      //  list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2019, 12, 2), );
+
     }
 
 
