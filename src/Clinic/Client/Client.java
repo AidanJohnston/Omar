bec80a96ccaaf95f47cc09934d540caab70f9014
Aidan Johnston <aidanjhnston@gmail.com>
2019-12-01 22:09:39 -0500

    Local is gone

--- src/Clinic/Client/Client.java
+++ src/Clinic/Client/Client.java
@@ -2,23 +2,26 @@ package Clinic.Client;
 
 import Clinic.Client.Connection.*;
 import Clinic.Client.GUI.*;
+import Clinic.Server.Data.DataReader;
 import Util.*;
 import Util.Exceptions.*;
 import Clinic.Core.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 @Deprecated
 public class Client {
 
-    public static void main(String args[]) throws ServerException {
+    public static void main(String args[]) throws Exception {
         ClientSecretary clientSecretary = new ClientSecretary();
 
         MyClient myClient = new MyClient("localhost", 6969, clientSecretary);
         clientSecretary.setMyClient(myClient);
-        Token a = clientSecretary.login("seans", "s");
+        Token a = clientSecretary.login("seand", "d");
 
-        ArrayList<Patient> list = clientSecretary.getPatientAll(a);
+        ArrayList<Appointment> list = clientSecretary.getAppointmentsCurrentDoctor(a.getUserID(), a);
         System.out.println(list);
     }
 }
