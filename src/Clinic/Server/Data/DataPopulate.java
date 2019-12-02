224cd8a4dcd11231174ba94a877751dec3533613
Aidan Johnston <aidanjhnston@gmail.com>
2019-12-01 19:44:55 -0500

    DataPopulate is done huza

--- src/Clinic/Server/Data/DataPopulate.java
+++ src/Clinic/Server/Data/DataPopulate.java
@@ -4,6 +4,7 @@ import Clinic.Core.*;
 import Util.UserType;
 import Util.Exceptions.LoginFailedException;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -11,10 +12,14 @@ public class DataPopulate {
     public static void main(String args[]) throws LoginFailedException {
         popDoc();
         popCreds();
+        popPatient();
+        popStaff();
+        popAppointment();
     }
 
     private static void popCreds(){
         ArrayList<Credentials> creds = new ArrayList<>();
+
         creds.add(new Credentials("seanp", "p", 1, UserType.PATIENT));
         creds.add(new Credentials("seand", "d", 2, UserType.DOCTOR));
         creds.add(new Credentials("seans", "s", 3, UserType.STAFF));
@@ -28,6 +33,7 @@ public class DataPopulate {
 
     private static void popDoc(){
         ArrayList<Doctor> list = new ArrayList<>();
+
         list.add(new Doctor("Orange", "Lube", LocalDate.of(1999, 5, 27), 420, "69 Hello Street", 129, "8076303284", "Fucking Bitches", "Head Doctor", 0, 2));
         list.add(new Doctor("Sean", "Dexor", LocalDate.of(1999, 7, 23), 403, "69 Suck my ass Street", 265, "87630483", "Fucking Biteches", "Second Doctor", 0, 1));
 
@@ -42,6 +48,7 @@ public class DataPopulate {
 
     private static void popPatient() {
         ArrayList<Patient> list = new ArrayList<>();
+
         list.add(new Patient("Dawson", "Aevo", LocalDate.of(2000, 3, 26 ), 696969, "420 oh heck yeah Street", 14, "80-7259803", 2525, LocalDate.of(2020, 1, 12), "ON"));
         list.add(new Patient("Aidan", "Johnston", LocalDate.of(1999, 5, 25), 2525, "Hey there this is an adress",  52352, "607-2343", 242, LocalDate.of(2020, 12, 23), "ON"));
 
@@ -54,19 +61,25 @@ public class DataPopulate {
         }
     }
 
-    private static  void popPrescriptions() {
-        ArrayList<Prescription>  list = new ArrayList<>();
+    private static  void popStaff() {
+        ArrayList<Staff> list = new ArrayList<>();
 
-        //list.add(new Prescription("Drug", LocalDate.now(), "This is a drug that cures being sick", "Take every  ten minutes", ))
+        list.add(new Staff("Rebeka", "Dismo", LocalDate.of(1954, 1, 14), 130, "43 HiThrer street", 123, "807-2415235"));
     }
+
 
     private static void popAppointment() {
-
         ArrayList<Appointment> list = new ArrayList<>();
-     //   list.add(new Appointment(new DataReader().readPatients().get(0), new DataReader().readDoctors().get(0), LocalDate.of(2019, 12, 2), new Diagnosis(), new Prescription()));
-      //  list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2019, 12, 2), );
 
+        list.add(new Appointment(new DataReader().readPatients().get(0), new DataReader().readDoctors().get(0), LocalDate.of(2020, 12, 2), new Diagnosis(), new Prescription()));
+        list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2019, 12, 2), new Diagnosis("Sick", LocalDate.now(), "You are sick"), new Prescription("Drug", LocalDate.now(), "Cures being sick", "Take once an hour")));
+
+        DataWriter d = new DataWriter();
+        //write some example info to the database xml
+        try {
+            d.writeAppointments(list);
+        } catch (Exception e) {
+            System.out.println("Writer broke");
+        }
     }
-
-
 }
