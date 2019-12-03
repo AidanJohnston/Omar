9bd8f021f2143677afe4675a3de52da4e1f3c5da
Aidan Johnston <aidanjhnston@gmail.com>
2019-12-02 22:50:04 -0500

    Test Classes
    Signed-off-by: Aidan Johnston <aidanjhnston@gmail.com>

--- src/Clinic/Client/TestClientSecreary.java
+++ src/Clinic/Client/TestClientSecreary.java
@@ -9,17 +9,22 @@ import Util.Exceptions.ServerException;
 import Util.RequestType;
 import Util.UserType;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import static org.hamcrest.CoreMatchers.is;
 
 public class TestClientSecreary {
     private Token token;
     private ClientSecretary clientSecretary;
+    private Logger logger;
     public TestClientSecreary() throws ServerException {
 
         this.clientSecretary = new ClientSecretary();
@@ -30,10 +35,15 @@ public class TestClientSecreary {
         this.token = clientSecretary.login("seans", "s");
     }
 
+    @Before
+    public void setupLogger() {
+        logger = Logger.getLogger(TestClientSecreary.class.getName());
+        logger.setLevel(Level.INFO);
+    }
 
     @Test
     public void testLogin() throws ServerException {
-        Assert.assertTrue(token.getType().equals(UserType.STAFF));
+        Assert.assertThat(token.getType(), is(UserType.STAFF));
     }
 
     @Test
@@ -42,15 +52,26 @@ public class TestClientSecreary {
 
         ArrayList<Appointment> list = new ArrayList<>();
 
-        list.add(new Appointment(new DataReader().readPatients().get(0), new DataReader().readDoctors().get(0), LocalDate.of(2020, 12, 2), "05:02", new Diagnosis(), new Prescription()));
-        list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2018, 12, 2), "13:30", new Diagnosis("Sick", LocalDate.of(2018, 12, 2), "You are sick"), new Prescription("Drug", LocalDate.of(2018, 12, 2), "Cures being sick", "Take once an hour")));
+        list.add(new Appointment(new DataReader().readPatients().get(0),
+                new DataReader().readDoctors().get(0),
+                LocalDate.of(2020, 12, 2),
+                "05:02",
+                new Diagnosis(),
+                new Prescription()));
+        list.add(new Appointment(new DataReader().readPatients().get(1),
+                new DataReader().readDoctors().get(1),
+                LocalDate.of(2018, 12, 2),
+                "13:30",
+                new Diagnosis("Sick",
+                        LocalDate.of(2018, 12, 2),
+                        "You are sick"),
+                new Prescription("Drug",
+                        LocalDate.of(2018, 12, 2),
+                        "Cures being sick",
+                        "Take once an hour")));
 
-        System.out.println(appointmentArrayList);
-        System.out.println(list);
-
-        System.out.println(list.equals(appointmentArrayList));
-
-        Assert.assertTrue(appointmentArrayList.equals(list));
+        Assert.assertEquals(appointmentArrayList.toString(), list.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
@@ -58,9 +79,19 @@ public class TestClientSecreary {
         ArrayList<Appointment> appointmentArrayList = clientSecretary.getAppointmentsDoctorAll(token, 2);
 
         ArrayList<Appointment> list = new ArrayList<>();
-        list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2018, 12, 2), "13:30", new Diagnosis("Sick", LocalDate.of(2018, 12, 2), "You are sick"), new Prescription("Drug", LocalDate.of(2018, 12, 2), "Cures being sick", "Take once an hour")));
+        list.add(new Appointment(new DataReader().readPatients().get(1),
+                new DataReader().readDoctors().get(1),
+                LocalDate.of(2018, 12, 2),
+                "13:30",
+                new Diagnosis("Sick",
+                        LocalDate.of(2018, 12, 2),
+                        "You are sick"), new Prescription("Drug",
+                LocalDate.of(2018, 12, 2),
+                "Cures being sick",
+                "Take once an hour")));
 
-        Assert.assertThat(appointmentArrayList, is(list));
+        Assert.assertEquals(appointmentArrayList.toString(), list.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
@@ -69,9 +100,9 @@ public class TestClientSecreary {
         ArrayList<Appointment> appointmentArrayList = clientSecretary.getCurrentAppointmentPatient(token, 52352);
 
         ArrayList<Appointment> list = new ArrayList<>();
-        list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2018, 12, 2), "13:30", new Diagnosis("Sick", LocalDate.of(2018, 12, 2), "You are sick"), new Prescription("Drug", LocalDate.of(2018, 12, 2), "Cures being sick", "Take once an hour")));
 
-        Assert.assertThat(appointmentArrayList, is(list));
+        Assert.assertEquals(appointmentArrayList.toString(), list.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
@@ -80,9 +111,15 @@ public class TestClientSecreary {
         ArrayList<Appointment> appointmentArrayList = clientSecretary.getAppointmentPatientAll(token, 14);
 
         ArrayList<Appointment> list = new ArrayList<>();
-        list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2018, 12, 2), "13:30", new Diagnosis("Sick", LocalDate.of(2018, 12, 2), "You are sick"), new Prescription("Drug", LocalDate.of(2018, 12, 2), "Cures being sick", "Take once an hour")));
+        list.add(new Appointment(new DataReader().readPatients().get(0),
+                new DataReader().readDoctors().get(0),
+                LocalDate.of(2020, 12, 2),
+                "05:02",
+                new Diagnosis(),
+                new Prescription()));
 
-        Assert.assertThat(appointmentArrayList, is(list));
+        Assert.assertEquals(appointmentArrayList.toString(), list.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
@@ -92,19 +129,51 @@ public class TestClientSecreary {
 
         ArrayList<Doctor> list = new ArrayList<>();
 
-        list.add(new Doctor("Aidan", "Johnston", LocalDate.of(1999, 5, 27), 420, "25 Street rd.", 129, "8076303284", "Surgery", "Head Doctor", LocalTime.of(9, 0), LocalTime.of(5, 0)));
-        list.add(new Doctor("Sean", "Dunlop", LocalDate.of(1999, 7, 23), 403, "658 Boulevard street", 2, "87630483", "Pharmacist", "Second Doctor", LocalTime.of(9, 0), LocalTime.of(5, 0)));
+        list.add(new Doctor("Aidan",
+                "Johnston",
+                LocalDate.of(1999, 5, 27),
+                420,
+                "25 Street rd.",
+                129,
+                "8076303284",
+                "Surgery",
+                "Head Doctor",
+                LocalTime.of(9, 0),
+                LocalTime.of(5, 0)));
+        list.add(new Doctor("Sean",
+                "Dunlop",
+                LocalDate.of(1999, 7, 23),
+                403,
+                "658 Boulevard street",
+                2,
+                "87630483",
+                "Pharmacist",
+                "Second Doctor",
+                LocalTime.of(9, 0),
+                LocalTime.of(5, 0)));
 
-        Assert.assertEquals(list, appointmentArrayList);
+        Assert.assertEquals(list.toString(), appointmentArrayList.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
     public void TestgetDoctorWithID() throws ServerException {
 
         Doctor appointmentArrayList = clientSecretary.getDoctorWithID(2, token);
-        Doctor doctor = new Doctor("Sean", "Dunlop", LocalDate.of(1999, 7, 23), 403, "658 Boulevard street", 2, "87630483", "Pharmacist", "Second Doctor", LocalTime.of(9, 0), LocalTime.of(5, 0));
+        Doctor doctor = new Doctor("Sean",
+                "Dunlop",
+                LocalDate.of(1999, 7, 23),
+                403,
+                "658 Boulevard street",
+                2,
+                "87630483",
+                "Pharmacist",
+                "Second Doctor",
+                LocalTime.of(9, 0),
+                LocalTime.of(5, 0));
 
-        Assert.assertEquals(doctor, appointmentArrayList);
+        Assert.assertEquals(doctor.toString(), appointmentArrayList.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
@@ -113,22 +182,40 @@ public class TestClientSecreary {
         ArrayList<Appointment> appointmentArrayList = clientSecretary.getAppointmentsCurrentDoctor(2, token);
 
         ArrayList<Appointment> list = new ArrayList<>();
-        list.add(new Appointment(new DataReader().readPatients().get(1), new DataReader().readDoctors().get(1), LocalDate.of(2018, 12, 2), "13:30", new Diagnosis("Sick", LocalDate.of(2018, 12, 2), "You are sick"), new Prescription("Drug", LocalDate.of(2018, 12, 2), "Cures being sick", "Take once an hour")));
 
-        Assert.assertEquals(list, appointmentArrayList);
+        Assert.assertEquals(list.toString(), appointmentArrayList.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
     public void TestgetPatientAll() throws ServerException {
 
-        ArrayList<Appointment> appointmentArrayList = clientSecretary.getAppointmentsCurrentDoctor(2, token);
+        ArrayList<Patient> appointmentArrayList = clientSecretary.getPatientAll(token);
 
         ArrayList<Patient> list = new ArrayList<>();
 
-        list.add(new Patient("Dawson", "Aevo", LocalDate.of(2000, 3, 26 ), 696969, "420 oh heck yeah Street", 14, "80-7259803", 2525, LocalDate.of(2020, 1, 12), "ON"));
-        list.add(new Patient("Aidan", "Johnston", LocalDate.of(1999, 5, 25), 2525, "Hey there this is an adress",  52352, "607-2343", 242, LocalDate.of(2020, 12, 23), "ON"));
+        list.add(new Patient("Dawson",
+                "Aevo",
+                LocalDate.of(2000, 3, 26 ),
+                696969,
+                "420 oh heck yeah Street",
+                14, "80-7259803",
+                2525,
+                LocalDate.of(2020, 1, 12),
+                "ON"));
+        list.add(new Patient("Aidan",
+                "Johnston",
+                LocalDate.of(1999, 5, 25),
+                2525,
+                "Hey there this is an adress",
+                52352,
+                "607-2343",
+                242,
+                LocalDate.of(2020, 12, 23),
+                "ON"));
 
-        Assert.assertEquals(list, appointmentArrayList);
+        Assert.assertEquals(appointmentArrayList.toString(), list.toString());
+        System.out.println("Test passed!\n");
     }
 
     @Test
@@ -137,18 +224,39 @@ public class TestClientSecreary {
         ArrayList<Doctor> appointmentArrayList = clientSecretary.searchDoctorName("Sean", token);
 
         ArrayList<Doctor> list = new ArrayList<>();
-        list.add(new Doctor("Sean", "Dunlop", LocalDate.of(1999, 7, 23), 403, "658 Boulevard street", 2, "87630483", "Pharmacist", "Second Doctor", LocalTime.of(9, 0), LocalTime.of(5, 0)));
+        list.add(new Doctor("Sean",
+                "Dunlop",
+                LocalDate.of(1999, 7, 23),
+                403,
+                "658 Boulevard street",
+                2,
+                "87630483",
+                "Pharmacist",
+                "Second Doctor",
+                LocalTime.of(9, 0),
+                LocalTime.of(5, 0)));
 
-        Assert.assertEquals(list, appointmentArrayList);
+        Assert.assertEquals(list.toString(), appointmentArrayList.toString());
+        System.out.println("Test passed!\n");
     }
     @Test
     public void TestsearchDoctorDate() throws ServerException {
 
         ArrayList<Doctor> appointmentArrayList = clientSecretary.searchDoctorDate(LocalDate.of(2018, 12 ,2), token);
         ArrayList<Doctor> list = new ArrayList<>();
-        list.add(new Doctor("Sean", "Dunlop", LocalDate.of(1999, 7, 23), 403, "658 Boulevard street", 2, "87630483", "Pharmacist", "Second Doctor", LocalTime.of(9, 0), LocalTime.of(5, 0)));
+        list.add(new Doctor("Aidan",
+                "Johnston",
+                LocalDate.of(1999, 5, 27),
+                420,
+                "25 Street rd.",
+                129,
+                "8076303284",
+                "Surgery",
+                "Head Doctor",
+                LocalTime.of(9, 0),
+                LocalTime.of(5, 0)));
 
-        Assert.assertEquals(list, appointmentArrayList);
+        Assert.assertEquals(list.toString(), appointmentArrayList.toString());
+        System.out.println("Test passed!\n");
     }
-
 }
